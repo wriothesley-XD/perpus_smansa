ef8c8009bdee62f21ddd9086b4491558dc7275af
bren <rinasafitri1981@gmail.com>
2026-09-12 12:27:58 +0700

    Replace headline font with Space Grotesk

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,10 +6,10 @@
 
         <title inertia>{{ config('app.name', 'Perpustakaan SMAN 1 Bukittinggi') }}</title>
 
-        <!-- Typography: Inter, Playfair Display, JetBrains Mono -->
+        <!-- Typography: Inter, Space Grotesk, JetBrains Mono -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=JetBrains+Mono:wght@400;600;700&family=Playfair+Display:ital,wght@0,600;0,700;0,800;1,600&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=JetBrains+Mono:wght@400;600;700&family=Space+Grotesk:wght@400;500;600;700&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
         @routes
